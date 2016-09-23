e3b413c013d3f33c358cb2ac261fdddee1038890
BrunoBob <tessierb95@gmail.com>
2016-09-22 21:36:53 -0700

    create server to connect to

--- server.c
+++ server.c
@@ -1,7 +1,41 @@
 #include "server.h"
 
 void launchServer(){
+  int serverSocket, clientSocket;                 // descriptor of sockets
+  struct sockaddr_in server_address; // for naming the server's listening socket
 
+  // create unnamed network socket for server to listen on
+  if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+    perror("Error creating socket");
+    exit(ERROR_CREATING_SOCKET);
+  }
+
+  // fills socket information
+  server_address.sin_family      = AF_INET;           // accept IP addresses
+  server_address.sin_addr.s_addr = htonl(INADDR_ANY); // accept clients on any interface
+  server_address.sin_port        = htons(PORT);       // port to listen on
+
+  // binding socket to a port
+  if (bind(server_socket, (struct sockaddr *)&server_address, sizeof(server_address)) == -1) {
+    perror("Error binding socket");
+    exit(ERROR_BIDING_SOCKET);
+  }
+
+  // listening for client connection
+  if (listen(server_socket, NUM_CONNECTIONS) == -1) {
+    perror("Error listening on socket");
+    exit(ERROR_LISTENING_SOCKET);
+  }
+
+  // server loop
+  while (1) {
+    // accept connection to client
+    if ((client_socket = accept(server_socket, NULL, NULL)) == -1) {
+      perror("Error accepting client");
+    } else {
+      printf("\nAccepted client\n");
+    }
+  }
 }
 
 void newClient(){
@@ -9,5 +43,6 @@ void newClient(){
 }
 
 int main(){
+  launchServer();
   return 0;
 }
--- server.h
+++ server.h
@@ -1,4 +1,4 @@
-//General includes
+ //General includes
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -11,8 +11,15 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
-#include <netdb.h>
+
+#define SERVER_IP "127.0.0.1"
+#define PORT 2222
+
+#define ERROR_CREATING_SOCKET 1
+#define ERROR_BIDING_SOCKET 2
+#define ERROR_LISTENING_SOCKET 3
+
+#define NUM_CONNECTIONS 1
 
 void launchServer();
 
